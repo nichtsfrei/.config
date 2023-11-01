44c83ce4e6a5610771ffe6c317900ea32f7586e5
Philipp Eder <philipp.eder@posteo.net>
2023-11-01 22:00:21 +0100

    add user commands

---
+++ user_commands.bash
@@ -0,0 +1,15 @@
+ #/bin/sh
+
+pacman --noconfirm -S \
+     steam \
+     kitty \
+     helix \
+     rustup \
+     qmk \
+     git \
+     curl \
+     cmake \
+     libnet \
+     hiredis \
+     gopls \
+     clang 
