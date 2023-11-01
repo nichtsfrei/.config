775c1cc40f37b75402db7210fad24271c99bad1c
Philipp Eder <philipp.eder@posteo.net>
2023-11-01 23:47:55 +0100

    Clean up

--- user_commands.bash
+++ user_commands.bash
@@ -20,7 +20,7 @@ git clone https://github.com/nichtsfrei/.config.git /tmp/.config
 for i in $(ls /home); do
   rm -rf "/home/$i/.config"
   cp -r "/tmp/.config" "/home/$i/.config"
-  chown -r $i:$i "/home/$i/.config"
+  chown -R $i:$i "/home/$i/.config"
   usermod -s /usr/bin/fish $i
 done
 rm -rf /tmp/.config
